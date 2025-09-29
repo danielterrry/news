8c9fd9dd5ca2d6f70e5054822ca2697fa131eeb3
Daniel <daniel.terry5@gmail.com>
2025-09-29 12:59:09 +1000

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var createError = require('http-errors');
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const cors = require('cors');
+
+var newsRouter = require('./routes/news');
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors({ origin: '*' }));
+
+app.use('/news', newsRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: req.app.get('env') === 'development' ? err : {},
+  });
+});
+
+module.exports = app;
